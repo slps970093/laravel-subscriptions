e109b6fc2c4f44d8a9234aeb7c7ca2e8679b27cb
小周 <slps970093@gmail.com>
2023-03-08 18:58:20 +0800

    [service provider] 更換 command register 方式

--- src/Providers/SubscriptionsServiceProvider.php
+++ src/Providers/SubscriptionsServiceProvider.php
@@ -24,9 +24,12 @@ class SubscriptionsServiceProvider extends ServiceProvider
      * @var array
      */
     protected $commands = [
-        MigrateCommand::class => 'command.rinvex.subscriptions.migrate',
-        PublishCommand::class => 'command.rinvex.subscriptions.publish',
-        RollbackCommand::class => 'command.rinvex.subscriptions.rollback',
+//        MigrateCommand::class => 'command.rinvex.subscriptions.migrate',
+//        PublishCommand::class => 'command.rinvex.subscriptions.publish',
+//        RollbackCommand::class => 'command.rinvex.subscriptions.rollback',
+        MigrateCommand::class,
+        PublishCommand::class,
+        RollbackCommand::class,
     ];
 
     /**
